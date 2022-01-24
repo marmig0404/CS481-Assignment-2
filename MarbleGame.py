0da8575e23c50bb44ca7833dc05b05c9a0392c3b
Martin Miglio <marmig0404@gmail.com>
2022-01-24 15:54:07 -0500

    Added function to allow 1D array for game state

--- MarbleGame.py
+++ MarbleGame.py
@@ -34,13 +34,29 @@ def visualize(board):
     print("")
 
 
+# changeArrayShape function:
+#   Changes shape of array from one dimensional (positions 0-14)
+#   to a triangular array for printing.
+
+def changeArrayShape(board_in_line):
+    triangularBoard = []
+    num_of_rows = 5
+    board_in_line_index = 0
+    for row_index in range(num_of_rows + 1):
+        row = []
+        for _ in range(row_index):
+            row.append(board_in_line[board_in_line_index])
+            board_in_line_index += 1
+        triangularBoard.append(row)
+    return triangularBoard
+
 # __main__ function:
 #   Example usage of the visualize function.
-
 if __name__ == '__main__':
     MARBLE = 'O'
     GAP = '_'
 
+    # Using triangular array
     gameBoard = [
         [MARBLE],
         [MARBLE, MARBLE],
@@ -49,3 +65,8 @@ if __name__ == '__main__':
         [MARBLE, MARBLE, MARBLE, MARBLE, MARBLE],
     ]
     visualize(gameBoard)
+
+    # Using single row array
+    gameBoard = [MARBLE, MARBLE, MARBLE, MARBLE, GAP, MARBLE, MARBLE,
+                 MARBLE, MARBLE, MARBLE, MARBLE, MARBLE, MARBLE, MARBLE, MARBLE]
+    visualize(changeArrayShape(gameBoard))
